2ceea0a6e67747effd5cdf649b2b7f8709b14b6f
Robert Petersson <rpetersson@gmail.com>
2021-02-21 19:10:26 +0100

    updated

---
+++ question-1.js
@@ -0,0 +1,5 @@
+function division(a, b) {
+    return a % b;
+}
+
+let result = (a,b) => a % b;
---
+++ question-2.js
@@ -0,0 +1,29 @@
+url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating"
+
+async function callAPI(url) {
+    let html = []
+    try {
+        const resultsContainer = document.querySelector(".results");
+
+        let response = await fetch(url);
+        let data = await response.json();
+        data = data["results"];
+        resultsContainer.innerHTML = "";
+        for (let i = 0; i <= 8; i++) {
+
+            let name = data[i].name
+            let rating = data[i].rating
+            let tags = data[i].tags.length
+
+            resultsContainer.innerHTML += '<div>' + name + '<br>' + rating + '<br>' + tags + '</div>';
+
+        }
+
+    } catch (error) {
+        console.log("An error occured");
+        console.log(error)
+    }
+}
+
+callAPI(url)
+
